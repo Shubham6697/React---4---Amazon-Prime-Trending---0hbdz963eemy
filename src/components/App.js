921942b1f2373533303b71f78f224bd7eb45617a
Shubham6697 <sg3477628@gmail.com>
2021-11-06 20:11:56 +0530

    Testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,35 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = (props) => {
+  const [curr, setCurr] = useState(0);
+  const len = props.slides.length;
   return (
-    <></>
+    <>
+      <div className="container">
+        <h1 data-testid="title">{props.slides[curr].title}</h1>
+        <p data-testid="text">{props.slides[curr].text}</p>
+
+      </div>
+
+    
+    {/* <h2>{props.slides.length}</h2> */}
+    
+    {
+      curr - 1 !== -1 ? <button onClick={()=>curr - 1 === -1 ? setCurr(0) : setCurr(curr-1)} data-testid="button-prev">Previous</button> : <button data-testid="button-prev" disabled>Previous</button>
+    }
+    
+    {
+      curr + 1 !== len ? <button onClick={()=>curr + 1 === len ? setCurr(len-1) : setCurr(curr+1)} data-testid="button-next">Next</button> : <button data-testid="button-next" disabled>Next</button>
+    }
+
+
+    {
+      curr !== 0 ? <button onClick={()=>setCurr(0)} data-testid="button-restart">Restart</button> : <button data-testid="button-restart" disabled>Restart</button>
+    }
+
+
+    </>
   )
 }
 
